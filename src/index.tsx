ea257d8f77db6e2008d58a5948f46ce45d4d4c2f
Damian Nowakowski <dnowakowski200@gmail.com>
2026-04-17 19:09:22 +0200

    refactor: removed commented rendering func

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
